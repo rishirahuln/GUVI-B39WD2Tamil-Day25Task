9a539af6a2730da4840ef4653d7838b86ad72b41
rishirahuln <nrishirahul@gmail.com>
2022-10-10 23:24:09 +0530

    first commit

---
+++ src/ListUsers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListUsers() {
+  return (
+    <div>
+      <img src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=1380&t=st=1665251104~exp=1665251704~hmac=9cf172cfc03c8b8b14b431da8503142640bbd674e1fd3773d25d3aa1bdf33d0f"/>
+    </div>
+  )
+}
+
+export default ListUsers
